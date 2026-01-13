f5c7650018417208482ee9dc6cbb24612c06722c
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2026-01-14 02:24:48 +0530

    groq chatbot

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+
+groq_api_key = st.secrets["GROQ_API_KEY"]
+
+st.set_page_config(
+    page_title="Chatbot",
+    page_icon="🤖",
+    layout="centered",
+)
+
+st.title("💬 Generative AI Chatbot")
+st.session_state.chat_history=[]
+for messages in st.session_state.chat_history:
+    with st.chat_message(messages["role"]):
+        st.markdown(messages["content"])
+        
+llm=ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0
+)
+user_prompt = st.chat_input("Ask Chatbot...")
+if user_prompt:
+    st.chat_message('user').markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    response = llm.invoke(
+        input = [{"role": "system", "content": "You are a helpful assistant"}, *st.session_state.chat_history]
+    )
+    assistant_response = response.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
